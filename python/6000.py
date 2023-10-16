cb6a33d24fe4558bc8d719e62972d8db098161db
Matthias Kreier <43933271+kreier@users.noreply.github.com>
2023-10-16 22:46:54 +0700

    using UTF-8 with Vietnamese and Russian

--- python/6000.py
+++ python/6000.py
@@ -4,48 +4,58 @@
 # Userguide https://www.reportlab.com/docs/reportlab-userguide.pdf 
 
 from reportlab.pdfgen import canvas
-from reportlab.lib.pagesizes import A4
 from reportlab.lib import colors
 from reportlab.lib.units import mm
+from reportlab.lib import colors
+from reportlab.graphics.shapes import *
+from reportlab.graphics import renderPDF
+
 
 import os
 
 # Some general settings
-
-filename = "p6000.pdf"
-page_width = 4*297*mm   # 4x A4 landscape
-# page_width  = 297*mm
-page_height = 210*mm    #    A4 landscape
+language = "en"
+filename = "p6000_" + language + ".pdf"
+page_width  = 297*mm   # 4x A4 landscape
+page_height = 210*mm     #    A4 landscape
 border_lr   = 10*mm
 border_tb   = 10*mm
 pdf_author  = "Matthias Kreier"
 pdf_title   = "6000 years human history visualized"
 pdf_subject = "Timeline of humankind"
 
-# database and language settings
-language = "en"
-db_adam_moses = "adam-moses_"
-
+# database settings
+db_adam_moses = "https://raw.githubusercontent.com/kreier/timeline/main/db/adam-joseph_" + language + ".csv"
 
 # Create the canvas
-
 c = canvas.Canvas(filename, pagesize=(page_width,page_height))
 c.setAuthor(pdf_author)
 c.setTitle(pdf_title)
 c.setSubject(pdf_subject)
 
 c.setFont("Helvetica", 4)
-c.drawString(border_lr + 5*mm, border_tb + 5*mm, "document created 2023-10-14")
+c.drawString(border_lr + 5*mm, border_tb + 5*mm, "document created 2023-10-15")
+
+
+d = Drawing(400, 200)
+d.add(Rect(50, 50, 300, 100, fillColor=colors.yellow))
+d.add(String(150,100, "Hello World", fontSize=18, fillColor=colors.red))
+special_characters = "Special characters Brüder việc với sứ đồ Phao-lô để"
+d.add(String(180,86, special_characters, fillColor=colors.red))
 
 # border around drawing area
+c.setLineWidth(0.7)
+c.setStrokeColorCMYK(1.00, 1.00, 0, 0.30) 
 x1 = border_lr
-x2 = page_width - border_lr
 y1 = border_tb
-y2 = page_height - border_tb
-c.line(x1, y1, x1, y2)
-c.line(x2, y1, x2, y2)
-c.line(x1, y1, x2, y1)
-c.line(x1, y2, x2, y2)
+c.rect(x1, y1, page_width - 2*border_lr, page_height - 2*border_tb, stroke=1, fill=0)
+
+# x2 = page_width - border_lr
+# y2 = page_height - border_tb
+# c.line(x1, y1, x1, y2)
+# c.line(x2, y1, x2, y2)
+# c.line(x1, y1, x2, y1)
+# c.line(x1, y2, x2, y2)
 
 # c.setFillColor(colors.grey)
 c.setFont("Helvetica-Bold", 20)
@@ -57,13 +67,14 @@ c.drawString(50, 400, "Timeline of 6000 years humankind")
 
 c.setFillColor(colors.black)
 c.setFont("Helvetica", 11)
-c.drawString(50, 360, "In this tutorial, we will demonstrate how to create PDF files using Python.")
-c.drawString(50, 340, "Python is a versatile programming language that can be used to create different types of files, including PDFs.")
-c.drawString(50, 320, "By the end of this tutorial, you will be able to generate PDF files using Python and the ReportLab library.")
+c.drawString(50, 360, "In this tutorial, Прискилла активно сотрудничали.")
+c.drawString(50, 340, "Brüder việc với sứ đồ Phao-lô đểerent types of files, including PDFs.")
+c.drawString(50, 320, "By the end of this tutoriausing Python and the ReportLab library.")
 
 # image_path = os.path.join(os.getcwd(), "python_logo.png")
 # c.drawImage(image_path, 50, 400, width=150, height=150)
 c.showPage()
 c.save()
+renderPDF.drawToFile(d, "drawing.pdf", "My drawing.")
     
 print(f"The file {filename} was created.")
