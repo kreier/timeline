f121c06640fbdd05f77c4a3e5a63746fd4dfa2d4
Matthias Kreier <43933271+kreier@users.noreply.github.com>
2023-10-17 13:55:08 +0700

    first successful axis

--- python/6000.py
+++ python/6000.py
@@ -8,11 +8,10 @@ from reportlab.lib import colors
 from reportlab.lib.units import mm
 from reportlab.graphics import renderPDF
 from reportlab.graphics.shapes import *
+from reportlab.graphics.charts.axes import XValueAxis
 from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.ttfonts import TTFont
 import os
-
-# font_path = os.path.join(os.getcwd(), "calibri.ttf")
 
 pdfmetrics.registerFont(TTFont('Aptos', 'aptos.ttf'))
 pdfmetrics.registerFont(TTFont('Aptos-bold', 'aptos-bold.ttf'))
@@ -20,7 +19,7 @@ pdfmetrics.registerFont(TTFont('Aptos-bold', 'aptos-bold.ttf'))
 # Some general settings
 language = "en"
 filename = "p6000_" + language + ".pdf"
-page_width  = 297*mm   # 4x A4 landscape
+page_width  = 4*297*mm   # 4x A4 landscape
 page_height = 210*mm     #    A4 landscape
 border_lr   = 10*mm
 border_tb   = 10*mm
@@ -46,38 +45,52 @@ y1 = border_tb
 c.rect(x1, y1, page_width - 2*border_lr, page_height - 2*border_tb, stroke=1, fill=0)
 
 # create coordinate system
+print("Create coordinate system")
+drawing_width  = page_width - 2 * border_lr
+drawing_height = page_height - 2 * border_tb
+d = Drawing(drawing_width, drawing_height) 
 
+# The drawing should span from 4050 BCE to 2050 CE, so we have to calculate
+# the length of one year in dots from drawing_with for this 6100 years
+year_dots = drawing_width / 6100
+
+# data = [(-4050, 2050)]
+xAxis = XValueAxis()
+xAxis.setPosition(0, 0, drawing_width)
+xAxis.valueMin = -4050
+xAxis.valueMax = 2050
+xAxis.valueStep = 100
+
+d.add(xAxis)
 
 # canvas.setFillColorCMYK(c, m, y, k) 
 # canvas.setStrikeColorCMYK(c, m, y, k) 
 # canvas.setFillColorRGB(r, g, b) 
 # c.setFillColor(colors.grey)
 
-c.setFont("Aptos-bold", 20)
-c.drawString(50, 400, "Timeline of 6000 years humankind")
+# c.setFont("Aptos-bold", 20)
+# c.drawString(50, 400, "Timeline of 6000 years humankind")
 
-c.setFillColor(colors.black)
-# c.setFont("Helvetica", 11)
-c.setFont('Aptos', 11)
-c.drawString(50, 360, "In this tutorial, Прискилла активно сотрудничали.")
-c.drawString(50, 340, "Brüder việc với sứ đồ Phao-lô đểerent types of files, including PDFs.")
-c.drawString(50, 320, "By the end of this tutoriausing Python and the ReportLab library.")
+# c.setFillColor(colors.black)
+# c.setFont('Aptos', 11)
+# c.drawString(50, 360, "In this tutorial, Прискилла активно сотрудничали.")
+# c.drawString(50, 340, "Brüder việc với sứ đồ Phao-lô đểerent types of files, including PDFs.")
+# c.drawString(50, 320, "By the end of this tutoriausing Python and the ReportLab library.")
 
-c.setFont("Aptos", 4)
-c.drawString(border_lr + 5*mm, border_tb + 5*mm, "document created 2023-10-15")
+# c.setFont("Aptos", 4)
+# c.drawString(border_lr + 5*mm, border_tb + 5*mm, "document created 2023-10-15")
 
 # Drawing
-d = Drawing(400, 200)
-d.add(Rect(50, 50, 300, 100, fillColor=colors.yellow))
-d.setFont("Aptos", 18)
-d.add(String(150,100, "Hello World", fontSize=18, fillColor=colors.red))
-special_characters = "Special characters Brüder việc với sứ đồ Phao-lô để"
-d.add(String(180,86, special_characters, fillColor=colors.red))
+# d = Drawing(400, 200)
+# d.add(Rect(50, 50, 300, 100, fillColor=colors.yellow))
+# d.add(String(150,100, "Hello World Brüder việc", fontSize=18, fontName="Aptos", fillColor=colors.red))
+# special_characters = "Special characters with German Brüder việc với sứ đồ Phao-lô để."
+# d.add(String(180,86, special_characters, fillColor=colors.blue, fontName="Aptos"))
 
 # image_path = os.path.join(os.getcwd(), "python_logo.png")
 # c.drawImage(image_path, 50, 400, width=150, height=150)
 
-renderPDF.draw(d, c, 0, 0)
+renderPDF.draw(d, c, border_lr, border_tb)
 c.showPage()
 c.save()
     
