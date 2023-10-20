3a75bcf6fcf6538885869adb520467bd6ac9af7c
Matthias Kreier <43933271+kreier@users.noreply.github.com>
2023-10-20 19:57:41 +0700

    first 24 persons imported and displayed

--- python/6000.py
+++ python/6000.py
@@ -12,7 +12,14 @@ from reportlab.graphics.charts.axes import XValueAxis
 from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.ttfonts import TTFont
 from reportlab.pdfbase.pdfmetrics import stringWidth
+import pandas as pd
+import datetime
 import os
+
+# Check execution location, exit if not in /python
+if os.getcwd()[-6:] != "python":
+    print("This script must be executed inside the python folder.")
+    exit()
 
 pdfmetrics.registerFont(TTFont('Aptos', 'aptos.ttf'))
 pdfmetrics.registerFont(TTFont('Aptos-bold', 'aptos-bold.ttf'))
@@ -37,7 +44,11 @@ c.setSubject(pdf_subject)
 # create drawing area
 drawing_width  = page_width - 2 * border_lr
 drawing_height = page_height - 2 * border_tb
-d = Drawing(drawing_width, drawing_height) 
+d = Drawing(drawing_width, drawing_height)
+x1 = border_lr
+y1 = border_tb
+x2 = x1 + drawing_width
+y2 = y1 + drawing_height
 
 # The drawing should span from 4075 BCE to 2075 CE, so we have to calculate
 # the length of one year in dots from drawing_with for this 6150 years
@@ -57,9 +68,6 @@ def create_horizontal_axis(c):
     # axis around drawing area
     c.setLineWidth(0.8)
     c.setStrokeColorCMYK(1.00, 1.00, 0, 0.50) 
-    x1 = border_lr
-    y1 = border_tb
-    y2 = y1 + drawing_height
     c.line(x1, y1, x1 + drawing_width, y1)
     c.line(x1, y2, x1 + drawing_width, y2)
 
@@ -68,6 +76,7 @@ def create_horizontal_axis(c):
     for i in range(61):
         # main tickmark
         tick_x = x1 + (75 + 100 * i) * dots_year
+        c.setLineWidth(1.0)
         c.line(tick_x, y1, tick_x, y1 - 2*mm)
         c.line(tick_x, y2, tick_x, y2 + 2*mm)
 
@@ -83,49 +92,51 @@ def create_horizontal_axis(c):
 
         # label the year
         year = str(abs((100 * i) - 4000))
-        offset_x = stringWidth(year, 'Aptos', 11) * 0.5
-        c.drawString(tick_x - offset_x, y1 - 16, year)
-        c.drawString(tick_x - offset_x, y2 + 8, year)
+        # offset_x = stringWidth(year, 'Aptos', 11) * 0.5
+        c.drawCentredString(tick_x, y1 - 16, year)
+        c.drawCentredString(tick_x, y2 + 8, year)
+
+        # vertical lines for centuries
+        c.setLineWidth(0.1)
+        c.line(tick_x, y1, tick_x, y2)
+
     c.drawString(x1, y1 - 16, "BCE")
     c.drawString(x1, y2 + 8 , "BCE")
+    # x_shift = stringWidth("CE", 'Aptos', 11)
+    c.drawRightString(x2, y1 - 16, "CE")
+    c.drawRightString(x2, y2 + 8, "CE")
+    c.setFont("Aptos", 4)
+    c.drawString(x1, y1 + 2, f"Timeline v3.0 - created {str(datetime.datetime.now())[0:16]}")
 
-    # c.rect(x1, y1, page_width - 2*border_lr, page_height - 2*border_tb, stroke=1, fill=0)
+def create_adam_moses(c):
+    # Import the persons with date of birth and death (estimated on October 1st) as pandas dataframe
+    print("Import data")
+    persons = pd.read_csv("../db/adam-moses.csv", encoding='utf8')
+    c.setFont("Aptos", 11)
+    c.setLineWidth(0.3)
+    for index, row in persons.iterrows():
+        born = int(row.born[0:4])
+        died = int(row.died[0:4])
+        if index < 14:
+            details = f"{row.person} {born} to {died} BCE - {born - died} years"
+            details_r = ""
+        else:
+            details = f"{row.person} "
+            details_r = f"{born} to {died} BCE - {born - died} years"
+        x_box = x1 + (4075 - born) * dots_year
+        y_box = y2 - index*16 - 16
+        x_boxwidth = (born - died) * dots_year
+        x_text = x_box + x_boxwidth * 0.5
+        if index < 10:
+            c.setFillColorRGB(0, 0, 1)
+        else:
+            c.setFillColorRGB(0, 0.7, 0)
+        c.rect(x_box, y_box, x_boxwidth, 14, fill = 1)
+        c.setFillColorRGB(1, 1, 1)
+        c.drawCentredString(x_text, y_box + 3.5, details)
+        c.setFillColorRGB(0, 0, 0)
+        c.drawString(x_box + x_boxwidth + 2, y_box + 3.5, details_r)
 
-    # data = [(-4050, 2050)]
-    # xAxis = XValueAxis()
-    # xAxis.setPosition(0, 0, drawing_width)
-    # xAxis.valueMin = -4050
-    # xAxis.valueMax = 2050
-    # xAxis.valueStep = 100
-
-    # d.add(xAxis)
-
-# canvas.setFillColorCMYK(c, m, y, k) 
-# canvas.setStrikeColorCMYK(c, m, y, k) 
-# canvas.setFillColorRGB(r, g, b) 
-# c.setFillColor(colors.grey)
-
-# c.setFont("Aptos-bold", 20)
-# c.drawString(50, 400, "Timeline of 6000 years humankind")
-
-# c.setFillColor(colors.black)
-# c.setFont('Aptos', 11)
-# c.drawString(50, 360, "In this tutorial, Прискилла активно сотрудничали.")
-# c.drawString(50, 340, "Brüder việc với sứ đồ Phao-lô đểerent types of files, including PDFs.")
-# c.drawString(50, 320, "By the end of this tutoriausing Python and the ReportLab library.")
-
-# c.setFont("Aptos", 4)
-# c.drawString(border_lr + 5*mm, border_tb + 5*mm, "document created 2023-10-15")
-
-# Drawing
-# d = Drawing(400, 200)
-# d.add(Rect(50, 50, 300, 100, fillColor=colors.yellow))
-# d.add(String(150,100, "Hello World Brüder việc", fontSize=18, fontName="Aptos", fillColor=colors.red))
-# special_characters = "Special characters with German Brüder việc với sứ đồ Phao-lô để."
-# d.add(String(180,86, special_characters, fillColor=colors.blue, fontName="Aptos"))
-
-# image_path = os.path.join(os.getcwd(), "python_logo.png")
-# c.drawImage(image_path, 50, 400, width=150, height=150)
 
 def render_to_file():
     renderPDF.draw(d, c, border_lr, border_tb)
@@ -136,4 +147,21 @@ def render_to_file():
 if __name__ == "__main__":
     import_data(text)
     create_horizontal_axis(c)
+    create_adam_moses(c)
     render_to_file()
+
+
+# inspiration bin
+
+# canvas.setFillColorCMYK(c, m, y, k) 
+# canvas.setStrikeColorCMYK(c, m, y, k) 
+# canvas.setFillColorRGB(r, g, b) 
+# c.setFillColor(colors.grey)
+
+
+# Drawing
+# d = Drawing(400, 200)
+# d.add(Rect(50, 50, 300, 100, fillColor=colors.yellow))
+# d.add(String(150,100, "Hello World Brüder việc", fontSize=18, fontName="Aptos", fillColor=colors.red))
+# special_characters = "Special characters with German Brüder việc với sứ đồ Phao-lô để."
+# d.add(String(180,86, special_characters, fillColor=colors.blue, fontName="Aptos"))
