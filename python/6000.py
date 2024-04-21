018a528cb8056b4639f5d7fc33d21667ab84f6fa
Matthias Kreier <43933271+kreier@users.noreply.github.com>
2024-04-22 00:27:52 +0700

    add support for Tagalog or Filipino

--- python/6000.py
+++ python/6000.py
@@ -68,6 +68,7 @@ supported = {"ar": "Arabic (العربية)",
              "yue": "Chinese Cantonese (Simplified) [中文简体（广东话）]",
              "si": "Sinhala (සිංහල)",
              "th": "Thai (ภาษาไทย)",
+             "tl": "Tagalog",
              "vi": "Vietnamese (Tiếng Việt)"}
 
 def create_dictionary(target_language):
