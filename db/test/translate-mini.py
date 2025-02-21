c76e44ee8fd68ec5d7f76ce7784efffe73826243
Matthias Kreier <43933271+kreier@users.noreply.github.com>
2025-02-21 18:39:13 +0700

    extend translations from 446 to 475 keys

--- db/test/translate-mini.py
+++ db/test/translate-mini.py
@@ -1,10 +1,8 @@
 # Create a google translated dictionary as starting point for a new language
-# It does not work with https://pypi.org/project/googletrans/ 3.0.0 (latest version)
-# You need the 3.1.0a0 alpha version from 2020
-# pip3 install googletrans==3.1.0a0
-# https://pypi.org/project/googletrans/3.1.0a0/ 
+# It works again in 2025 with https://pypi.org/project/googletrans/ 4.0.2 (latest version) and asyncio
 
 import os, sys
+import asyncio
 import pandas as pd
 from googletrans import Translator
 
@@ -25,15 +23,30 @@ def check_existing(language, filename):
             print("Invalid input. Please enter 'yes' or 'no'.")
             exit()
     else:
-        print(f"Creating a new mini_{language}.tsv file.")
+        print(f"Creating a new mini_{language}.csv file.")
 
 def import_english():
     global dict
-    print("Import english mini file: ", end="")
-    dict = pd.read_csv("./mini_en.tsv", sep="\t")
+    print("Import english mini dictionary file: ", end="")
+    # dict = pd.read_csv("./mini_en.tsv", sep="\t")
+    dict = pd.read_csv("./mini_en.csv")
     dict = dict.fillna(" ")
     print(f"found {len(dict)} entries.")
     print(dict)
+
+async def translate_dictionary():
+    global dict_translated
+    global number_characters  
+    async with Translator() as translator:
+        for index, row in dict_translated.iterrows(): # with 3 columns 'key' 'text' and 'english'
+            english_text = row.english
+            number_characters += len(english_text)
+            if not english_text == " ": # it only applies to row with empty strings - causes translation error
+                result = await translator.translate(english_text, src='en', dest=language)
+                dict_translated.at[index, 'text'] = result.text
+                # print('.', end='')
+            if (index + 1) % 40 == 0:
+                print(f" {index}")
 
 if __name__ == "__main__":
     dict = pd.DataFrame() # will contain the english dictionary with 'key' and 'text' column, plus 'alternative' and 'notes' (not used)
@@ -41,7 +54,7 @@ if __name__ == "__main__":
         print("You did not provide a language as argument. Put it as a parameter after the program name.")
         exit()
     language = sys.argv[1]
-    filename = "./mini_" + language + ".tsv"
+    filename = "./mini_" + language + ".csv"
     print(f"You want to translate to {language}.")
     check_existing(language, filename)
     import_english()
@@ -49,15 +62,9 @@ if __name__ == "__main__":
     dict_translated = dict[['key', 'text']].copy()   # create a new dictionary, copy columns key and text
     dict_translated['english'] = dict['text'].copy() # add a column 'english' and fill with 'text' from english dictionary
     print("\nTranslating ...")
-    translator = Translator()
-    for index, row in dict_translated.iterrows(): # with 3 columns 'key' 'text' and 'english'
-        english_text = row.english
-        if not english_text == " ": # it only applies to row with empty strings - causes translation error
-            dict_translated.at[index, 'text'] = translator.translate(english_text, src='en', dest=language).text
-            print('.', end='')
-        if (index + 1) % 40 == 0:
-            print(f" {index}")
-
+    number_characters = 0      # you can translate up to 500,000 characters per month for free        
+    asyncio.run(translate_dictionary())
     print(dict_translated)
     print("Exporting ...")
-    dict_translated.to_csv(filename, index=False, sep="\t")
+    dict_translated.to_csv(filename, index=False)
+    print(f"You translated {number_characters} characters.")
