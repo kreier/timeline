3a922c5cee699fb565bfbc7b74bdc27328c02270
Matthias Kreier <43933271+kreier@users.noreply.github.com>
2025-04-17 21:45:27 +0700

    updated Tagalog

---
+++ db/compare.py
@@ -0,0 +1,19 @@
+# Compare two dictionaries for their entries in column key, english and notes
+# The two arguments are the two languages to compare
+#
+# Structure: 
+#  Google docs:    key, text, english, notes - current_key, current_text, current_english, current_notes
+#  dictionary csv: key, text, english, notes 
+#  reference csv:  key, text, english, notes
+# reference dictionary: key, text, alternative, notes
+
+
+
+def second_func():
+    print("Import reference english dictionary")
+    print("Check if target language already exists")
+    print("start a dialog: Do you want to overwrite?")
+    print("Auto translation done. Translated x key phrases.")
+
+if __name__ == "__main__":
+    print("This is the main function")
